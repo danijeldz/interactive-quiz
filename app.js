f6799ea7fca332e472ee320d1e50418f12ca04bb
danijeldz <danijeldzankic@hotmail.com>
2020-04-17 14:53:35 +0200

    added counting score effect with setInterval. added scrollTo to move view back to top after submit event

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const correctAnswers = ["B", "B", "B", "B"]
+const correctAnswers = ["B", "B", "B", "A", "B"]
 const form = document.querySelector(".quiz-form")
 const result = document.querySelector(".result")
 //const resultSpan = document.querySelector("span.text-primary")
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
-  let answers = [e.target.q1.value, e.target.q2.value, e.target.q3.value, e.target.q4.value]
+  let answers = [e.target.q1.value, e.target.q2.value, e.target.q3.value, e.target.q4.value, e.target.q5.value]
 
   //calculate score
   let score = 0
@@ -15,10 +15,21 @@ form.addEventListener("submit", (e) => {
       score += 1
     }
   })
-  scorePercent = (score / 4) * 100
+  scorePercent = (score / 5) * 100
 
   //show score
-  result.querySelector("span").textContent = `${scorePercent}%`
+  //result.querySelector("span").textContent = `${scorePercent}%`
   result.classList.remove("d-none")
   result.classList.add("d-block")
+  scrollTo(0, 0)
+
+  let scoreCounter = 0
+  let timer = setInterval(() => {
+    result.querySelector("span").textContent = `${scoreCounter}%`
+    if (scoreCounter == scorePercent) {
+      clearInterval(timer)
+    } else {
+      scoreCounter++
+    }
+  }, 10)
 })
